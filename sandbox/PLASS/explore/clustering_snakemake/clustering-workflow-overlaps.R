dc9f05daf420490f9437f00870adc66e35bdbec3
Taylor Reiter <taylorreiter@gmail.com>
2018-09-27 07:27:18 -0700

    accomodate domains less than 200 residues

--- sandbox/PLASS/explore/clustering_snakemake/clustering-workflow-overlaps.R
+++ sandbox/PLASS/explore/clustering_snakemake/clustering-workflow-overlaps.R
@@ -16,23 +16,40 @@ dom <- makeGRangesFromDataFrame(dom,
                                 #strand.field="strand",
                                 starts.in.df.are.0based=FALSE)
 
-# define 200 bp interval to calc pid with based on max coverage
-cov <- coverage(dom)
-mid <- which.max(cov)
-lower <- mid - 100
-upper <- mid + 100
-
 # define reads to keep for pid calc ------------------------------------
 
-# define domain_name using dom object
-# start and end based on cov profile
+if(length(dom) > 211){
+  # if the domain is greater than 211 residues:
+  # make a sliding window of 200bp that progresses by 10 bp each step. 
+  # Calc overlaps for each of these. 
+  # Select the one that is longest at the end. 
+  ranges <- seq(from = 1, to = (max(dom$domain_len) - 200), by = 10)
+  all <- list()
 
-# make a sliding window of 200bp that progresses by 10 bp each step. Calc overlaps for each of these. Select the one that is longest at the end. 
-ranges <- seq(from = 1, to = (max(dom$domain_len) - 200), by = 10)
-all <- list()
-
-for(start in ranges){
-  end = start + 200
+  for(start in ranges){
+    end = start + 200
+    want <- makeGRangesFromDataFrame(data.frame(domain_name = as.character(seqnames(dom))[1], 
+                                                start = start, end = end), 
+                                     keep.extra.columns=T,
+                                     ignore.strand=T,
+                                     seqinfo=NULL,
+                                     seqnames.field=c("domain_name"),
+                                     start.field="start",
+                                     end.field="end",
+                                     #strand.field="strand",
+                                     starts.in.df.are.0based=FALSE)
+    ov <- subsetByOverlaps(dom, want, minoverlap = 125)
+    all[[length(all)+1]] <- ov
+  }
+  most <- which.max(sapply(all, length)) # find the granges object with the most records
+  print(paste0("200 aa region with most overlapping seqs starts at residue ", ranges[most]))
+  write.table(all[[most]]$query_name, file = snakemake@output[['keep']], 
+              quote = F, row.names = F, col.names = F) # write sequence names to a file
+} else {
+  # if the domain is not greater than 211 bp:
+  # subset to aa seqs that have at least 125 residues overlapping the domain
+  start = 1
+  end = length(dom)
   want <- makeGRangesFromDataFrame(data.frame(domain_name = as.character(seqnames(dom))[1], 
                                               start = start, end = end), 
                                    keep.extra.columns=T,
@@ -44,13 +61,9 @@ for(start in ranges){
                                    #strand.field="strand",
                                    starts.in.df.are.0based=FALSE)
   ov <- subsetByOverlaps(dom, want, minoverlap = 125)
-  all[[length(all)+1]] <- ov
+  print("Domain not long enough to select area with most coverage.")
+  write.table(ov$query_name, file = snakemake@output[['keep']], 
+              quote = F, row.names = F, col.names = F) # write sequence names to a file
 }
 
-# find the granges object with the most records
-most <- which.max(sapply(all, length))
 
-print(paste0("200 aa region with most overlapping seqs starts at residue ", ranges[most]))
-
-# write sequence names to a file
-write.table(all[[most]]$query_name, file = snakemake@output[['keep']], quote = F, row.names = F, col.names = F)
