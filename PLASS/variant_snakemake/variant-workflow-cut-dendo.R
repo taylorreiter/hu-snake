6dadc5f4f230f89a93f4855523cb213213462b12
Taylor Reiter <taylorreiter@gmail.com>
2018-10-22 15:14:31 -0700

    update to working pipeline

--- PLASS/variant_snakemake/variant-workflow-cut-dendo.R
+++ PLASS/variant_snakemake/variant-workflow-cut-dendo.R
@@ -10,18 +10,15 @@ dend <- as.dendrogram(hclust(dist(mat), method = "ave"))
 labels(dend) <- gsub("outputs.bam_subsets.", "", labels(dend))
 labels(dend) <- gsub(".fa", "", labels(dend))
 
-plt <- fviz_nbclust(mat, hcut, method = "silhouette",
-                    hc_method = "complete")
-k <- which.max(plt$data$y)
-groups <- cutree(dend, k = k)
+groups <- cutree(dend, h = 2)
 
 # plot dendogram
 #dend1 <- color_branches(dend, k = k)
 #dend1 <- color_labels(dend1, k = k)
 #plot(dend1, main = "default cuts by cutree")
 
-for(i in 1:k){
+for(i in unique(groups)){
   group <- names(groups[groups == i])
   write.table(group, file = paste0("outputs/comp/cut", i, ".txt"), 
               quote = F, row.names = F, col.names = F)
-  }
+}
